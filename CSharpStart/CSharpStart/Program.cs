ed9083486f671bffeec519c4166cfcea769bf509
Projeto - DEV <lokluis@gmail.com>
2018-08-25 20:20:01 -0300

    Primeiro Commit

--- CSharpStart/CSharpStart/Program.cs
+++ CSharpStart/CSharpStart/Program.cs
@@ -1,12 +1,10 @@
 ﻿using System;
 
-namespace CSharpStart
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace CSharpStart {
+    class Program {
+        static void Main(string[] args) {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome C#!");
         }
     }
 }
